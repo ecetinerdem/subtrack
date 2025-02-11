4d61e26b15523d4c5c545d47f23f074c7d2450f3
Cetin Erdem <cetoerdem1@gmail.com>
2025-02-11 22:13:43 +0400

    Update user.routes.js

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -5,5 +5,7 @@ const userRouter = Route();
 userRouter.get('/', (req, res) => res.send({ title: 'GET all users'}));
 userRouter.get('/:id', (req, res) => res.send({ title: 'GET user details'}));
 userRouter.post('/', (req, res) => res.send({ title: 'POST new user'}));
-userRouter.get('/:id', (req, res) => res.send({ title: 'UPDATE a user'}));
-userRouter.delete('/:id', (req, res) => res.send({ title: 'DELETE a user'}));
+userRouter.get('/:id', (req, res) => res.send({ title: 'UPDATE user'}));
+userRouter.delete('/:id', (req, res) => res.send({ title: 'DELETE user'}));
+
+export default userRouter;
