91a15115c78cb5d10ff0e807804505a2d8179a24
Cetin Erdem <cetoerdem1@gmail.com>
2025-02-13 01:22:21 +0400

    Update mongodb.js

--- database/mongodb.js
+++ database/mongodb.js
@@ -2,5 +2,18 @@ import mongoose from 'mongoose';
 import { DB_URI, NODE_ENV } from '../config/env';
 
 if(!DB_URI) {
-    throw new Error('Please define the MONGODB_URI in your environment variables.');
+    throw new Error('Please define the MONGODB_URI in your environment variables inside .env.<development/production>.local');
 }
+
+const connectDatabase = async () => {
+    try {
+        await mongoose.connect(DB_URI);
+
+    } catch (error) {
+        console.log("Error connecting database: ", error);
+
+        process.exit(1);
+    }
+}
+
+export default connectDatabase;
