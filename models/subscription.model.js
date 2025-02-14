23e2894001003c3dc746ece903a325a144cd59e1
Cetin Erdem <cetoerdem1@gmail.com>
2025-02-14 19:59:38 +0400

    Update subscription.model.js

---
+++ models/subscription.model.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+
+const subscriptionSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "Subscription name is required"],
+        trim: true,
+        minLength: 2,
+        maxLength: 100,
+    },
+    price: {
+        type: Number,
+        required: [true, "Subscription price is required"],
+        min: [0, "Price must be greater than 0"],
+    },
+    currency: {
+        type: String,
+        enum: ["USD", "EUR", "GBP", "TRY"],
+    }
+}, 
+{
+    timestamps: true
+});
