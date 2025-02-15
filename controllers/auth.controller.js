12601cb0b123795b945f9e70e464bd208d09b39a
Cetin Erdem <cetoerdem1@gmail.com>
2025-02-15 23:51:27 +0400

    Update auth.controller.js

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,5 +1,50 @@
+import mongoose from 'mongoose';
+import User from '../models/user.model.js'
+import bcrypt from 'bcryptjs'
+import jwt from 'jsonwebtoken'
+
+
+
+
 export const signUp = async (req, res, next) => {
-    //ımplement signup
+    const session = await mongoose.startSession();
+    session.startTransaction();
+
+    try {
+        const {name, email, password } = req.body;
+
+        const existingUser = await User.findOne({ email });
+        if (existingUser) {
+            const error = new Error('User already exist');
+            error.statusCode = 409;
+            throw error;
+        }
+
+        // Hash password
+        const salt = await bcrypt.genSalt(10)
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        const newUsers = await user.create([{
+            name,
+            email,
+            password: hashedPassword
+        }], { session });
+
+        const token = jwt.sign({ userId: newUsers[0]._id }, JWT_SECRET, {expiresIn: JWT_EXPIRES})
+
+        await session.commitTransaction();
+        session.endSession();
+
+        res.status(201).json({
+            success: true,
+            message: "User created successfully",
+        })
+
+    } catch (error) {
+        await session.abortTransaction();
+        session.endSession();
+        next(error);
+    }
 }
 
 
