0b65da7af02d964583447c17fdb3d3e28bde525b
Cetin Erdem <cetoerdem1@gmail.com>
2025-02-15 11:41:49 +0400

    Update subscription.model.js

--- models/subscription.model.js
+++ models/subscription.model.js
@@ -43,20 +43,55 @@ const subscriptionSchema = new mongoose.Schema({
         type: Date,
         required: true,
         validate: {
-            validator: (value) => value <= new Date(),
+            validator: function (value) { 
+                return value <= new Date();
+            },
             message: "Start date must be in the past",
         }
     },
     renewalDate: {
         type: Date,
-        required: true,
         validate: {
-            validator: (value) => value > new Date(),
-            message: "Renewal date must be in the future",
+            validator: function (value) { 
+                return value > this.startDate;
+            },
+            message: "Renewal date must be after the startdate",
         }
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+        index: true,
     }
 
 }, 
 {
     timestamps: true
 });
+
+
+//Auto-calculate renewal date, if missing
+subscriptionSchema.pre("save", function () {
+    if (!this.renewalDate) {
+        const renewalPeriods = {
+            daily: 1,
+            weekly: 7,
+            monthly: 30,
+            yearly: 365,
+        };
+        this.renewalDate = new Date(this.startDate);
+        this.renewalDate.setDate(this.renewalDate.getDate() + renewalPeriods[this.frequency]); 
+    }
+    //Auto-update the status if renewal date has passed
+    if (this.renewalDate < new Date()) {
+        this.status = "expired"
+    }
+    
+    next();
+
+})
+
+
+
+
