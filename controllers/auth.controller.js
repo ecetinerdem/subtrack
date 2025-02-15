f8d75dc4579410196721a61f8c229a53743913cd
Cetin Erdem <cetoerdem1@gmail.com>
2025-02-15 23:30:06 +0400

    Create auth.controller.js

---
+++ controllers/auth.controller.js
@@ -0,0 +1,13 @@
+export const signUp = async (req, res, next) => {
+    //ımplement signup
+}
+
+
+export const signIn = async (req, res, next) => {
+    //ımplement signin
+}
+
+
+export const signOut = async (req, res, next) => {
+    //ımplement signout
+}
