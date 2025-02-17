b6472a1d2f1c77d7042a0d1ce51a5da2a849e4c9
Cetin Erdem <cetoerdem1@gmail.com>
2025-02-17 23:09:58 +0400

    Update user.routes.js

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,9 +1,10 @@
 import { Router} from "express";
+import { getUser, getUsers } from "../controllers/user.controller";
 
-const userRouter = Route();
+const userRouter = Router();
 
-userRouter.get('/', (req, res) => res.send({ title: 'GET all users'}));
-userRouter.get('/:id', (req, res) => res.send({ title: 'GET user details'}));
+userRouter.get('/', getUsers);
+userRouter.get('/:id', getUser);
 userRouter.post('/', (req, res) => res.send({ title: 'POST new user'}));
 userRouter.get('/:id', (req, res) => res.send({ title: 'UPDATE user'}));
 userRouter.delete('/:id', (req, res) => res.send({ title: 'DELETE user'}));
