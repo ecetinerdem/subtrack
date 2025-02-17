42c9632d0730b49b260df75d7fd7a527bd29f018
Cetin Erdem <cetoerdem1@gmail.com>
2025-02-17 23:13:09 +0400

    Update auth.middleware.js

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -1,3 +1,7 @@
+import { JWT_SECRET } from "../config/env";
+import jwt from 'jsonwebtoken'
+import User from "../models/user.model";
+
 const authorize = async (req, res, next)  => {
     try {
         let token;
@@ -8,6 +12,14 @@ const authorize = async (req, res, next)  => {
 
         if (!token) return res.status(401).json({ message: 'Unauthorized'})
 
+            const decoded = jwt.verify(token, JWT_SECRET)
+            const user = await User.findById(decoded.userId)
+
+            if (!user) return res.status(401).json({ message: 'Unauthorized' })
+
+            req.user = user
+
+            next()
     } catch (error) {
         res.status(401).json({ message: 'Unauthorized', error: error.message });
     }
