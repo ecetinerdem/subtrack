02ec4bb339b37651d36c1df99bac7a5e5944295c
Cetin Erdem <cetoerdem1@gmail.com>
2025-02-26 18:56:15 +0400

    Update workflow.controller.js

--- controllers/workflow.controller.js
+++ controllers/workflow.controller.js
@@ -6,12 +6,14 @@ const { serve } = require('@upstash/workflow/express')
 export const sendReminders = serve(async (context) => {
     const { subscriptionId } = context.requestPayload;
     const subscription = await fetchSubscription(context, subscriptionId);
+
+    if(!subscription || subscription.status !== active) return;
 });
 
 
 const fetchSubscription = async (context, subscriptionId) => {
     return await context.run('get subscription', () => {
         return Subscription.findById(subscriptionId).populate('user', 'name email')
-    } )
+    })
 }
 
