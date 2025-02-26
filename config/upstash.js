28d0f5c3180bfb1adeab9a1409dbc1be87db3834
Cetin Erdem <cetoerdem1@gmail.com>
2025-02-26 18:42:04 +0400

    Update upstash.js

--- config/upstash.js
+++ config/upstash.js
@@ -1,2 +1,9 @@
 import { Client as WorkflowClient } from '@upstash/workflow';
 
+import { QSTASH_TOKEN, QSTASH_URL } from './env';
+
+
+export const WorkflowClient = new WorkflowClient({
+    token: QSTASH_TOKEN,
+    baseUrl: QSTASH_URL,
+})
